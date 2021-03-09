3746c999ac8bcd6cc84dd9dac4f8e53045290a6e
Linas Jonika <linas.jonika1@gmail.com>
2021-03-09 23:38:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,62 +1,122 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
 import React from 'react';
-import './App.scss'
-// import { useForm } from "react-hook-form";
-import { useState } from 'react'
+import './App.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', searchResults: []};
+    this.state = {
+      value: '',
+      searchResults: [],
+      isSearchOpen: false,
+      selectedMovie: undefined,
+      timeout: null,
+      isLoading: false,
+      errorMessage: '',
+    };
   }
   handleChange = (event) => {
-    this.setState({value: event.target.value});
-    this.getSearchOptions(event.target.value);
-  }
+    this.setState({ value: event.target.value });
+    if (event.target.value && event.target.value.length >= 3) {
+      this.debounceSearch(event.target.value);
+    } else {
+      this.setState({ searchResults: [] });
+    }
+  };
+
   handleSubmit = (event) => {
     alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
-  }
+  };
+  openAutoSuggest = () => {
+    this.setState({ isSearchOpen: true });
+  };
+  selectMovie = (movie) => {
+    this.setState({ selectedMovie: movie });
+    this.setState({ isSearchOpen: false });
+  };
+  debounceSearch = (query) => {
+    clearTimeout(this.state.timeout);
+    const timeout = setTimeout(() => {
+      this.getSearchOptions(query);
+    }, 2000);
+    this.setState({ timeout });
+  };
   getSearchOptions = (query) => {
-    // process.env.MOVIEDB_KEY
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${'d1ec5e658afc08e9a0262107d6581ada'}&language=en-US&query=${query}`
-    fetch(url,{
-      method:'GET'
-  }).then(response => response.json()).then(data => {
-    this.setState({
-      searchResults: data.results
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${'d1ec5e658afc08e9a0262107d6581ada'}&language=en-US&query=${query}`;
+    this.setState({ isLoading: true });
+    fetch(url, {
+      method: 'GET',
     })
-    
-    console.log(data)
-  })
-  }
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          searchResults: data.results.slice(0, 8),
+        });
+        console.log(data);
+        this.setState({ isLoading: false });
+      })
+      .catch((error) => {
+        this.setState({ isLoading: false });
+        this.setState({ errorMessage: 'Could not find films' });
+      });
+  };
   render() {
-    console.log(process.env.MOVIEDB_KEY)
     return (
-    <div className="App">
-      <div className="Header">
-        <form>
-          <label>
-            <input type="text" name="name" value={this.state.value} onChange={this.handleChange} />
-          </label>
-        </form>
-      </div>
-
-      <div>
-        {this.state.searchResults.map(result => {
-          return (
-            <div>
-              <div className='Title'>{result.title}</div>
-              <div className='Rating'>{result.vote_average}</div>
-              <div className='Date'>{result.release_date.slice(0, 4)}</div>
+      <div className="App">
+        <div className="Header">
+          <div className="Search">
+            <div className="SearchBar">
+              <img className="MovieIcon" src="/movieWhite.svg" />
+              <button className="MovieName" onClick={this.openAutoSuggest}>
+                {this.state.selectedMovie
+                  ? this.state.selectedMovie.title
+                  : 'Enter movie name'}
+              </button>
             </div>
-            )
-          })}
+            <button className="SearchButton">
+              <img src="/search.svg" />
+            </button>
+            {this.state.isSearchOpen && (
+              <form className="AutoSuggestForm">
+                <div className="AutoSuggest">
+                  <img className="MovieIconBlack" src="/movieBlack.svg" />
+                  <label className="AutoSuggestLabel">
+                    <input
+                      autoComplete="off"
+                      className="Input"
+                      type="text"
+                      name="name"
+                      autoFocus
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                    Enter a movie name
+                  </label>
+                </div>
+                <div className="AutocompleteResults">
+                  {this.state.isLoading && <div>Loading...</div>}
+                  {this.state.searchResults.map((result) => {
+                    return (
+                      <div
+                        className="Results"
+                        key={result.id}
+                        onClick={() => this.selectMovie(result)}
+                      >
+                        <div className="Title">{result.title}</div>
+                        <div className="AutocompleteInfo">
+                          {result.vote_average} Rating,{' '}
+                          {result.release_date.slice(0, 4)}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </form>
+            )}
+          </div>
         </div>
-      {/* <div>{this.state.searchResults.map(x => x.vote_average)}</div> */}
-      <p>This pragraph</p>
-    </div>
+      </div>
     );
   }
 }
